4d2d1aefd737575abb1a26c2db4d1210ff66ed99
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-06-10 16:44:40 +0200

    Add componets

---
+++ components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import CardLeft from './CardLeft';
+import CardRight from './CardRight';
+
+const Card = ({ apps, totalapps }) => {
+    const cards = apps.map((app, index) => (
+        <div className="app-card" key={app.name}>
+            <CardLeft img={app.img} appno={index + 1} totalapps={totalapps} />
+            <CardRight app={app}/>
+        </div>
+    ));
+
+    return cards
+};
+
+
+export default Card;
