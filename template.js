83cf79fd711264ea80d12af1f09c0a083005d4df
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-06-10 16:52:06 +0200

    Add styles.css

--- template.js
+++ template.js
@@ -8,6 +8,7 @@ export default function template(title, initialState = {}, content = "") {
               <head>
                 <meta charset="utf-8">
                 <title> ${title} </title>
+                <link href="assets/styles.css" rel="stylesheet">
               </head>
               <body>
                 <div class="content">
