8250a6a32709b54474363cf951216b014cd93007
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-06-10 21:53:28 +0200

    Use jsx extension for components

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     }
 };
